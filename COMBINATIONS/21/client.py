e5c6b71a4fdfb5032af32e6ae44b27eef7ae1777
Aniket Sahu <148129052+aniketsahu28m@users.noreply.github.com>
2025-10-28 07:14:39 +0530

    Create client.py

---
+++ COMBINATIONS/21/client.py
@@ -0,0 +1,27 @@
+import socket
+
+def main():
+    s = socket.socket()
+    s.connect(('localhost', 5191))
+    while True:
+        menu = s.recv(4096).decode()
+        print(menu, end='')
+        choice = input()
+        s.sendall(choice.encode())
+        if choice == '1':
+            print(s.recv(4096).decode(), end='')
+            name = input()
+            s.sendall(name.encode())
+            print(s.recv(4096).decode(), end='')
+            n = input()
+            s.sendall(n.encode())
+            for _ in range(int(n)):
+                print(s.recv(4096).decode(), end='')
+                amt = input()
+                s.sendall(amt.encode())
+        elif choice == '2':
+            print(s.recv(8192).decode())
+        elif choice == '3':
+            break
+
+main()
